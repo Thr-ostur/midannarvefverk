61266d76486eae3f07e3e84033540c1c938b4099
ThrosturH3090 <trolli2000@gmail.com>
2018-03-05 21:02:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+from bottle import run, route, error, static_file, template
+import requests
+import os
 
+response = requests.get('http://apis.is/petrol')
+
+data = response.json()
+
+@route('/')
+def index():
+    return template('index', data=data)
+
+@route('/company/<c>')
+def company(c):
+    return template('company', c=c, data=data)
+
+@route('/details/<key>')
+def details(key):
+    return template('details', key=key, data=data)
+
+@route('/static/<filename>')
+def server_static(filename):
+    return static_file(filename, root='./')
+
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
