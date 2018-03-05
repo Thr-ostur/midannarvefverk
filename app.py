e0af9b86c6e3bfdfe4e1011a438e650eee65da85
ThrosturH3090 <trolli2000@gmail.com>
2018-03-05 22:57:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,52 @@
-from bottle import run, route, error, static_file, template
-import requests
-import os
+from bottle import run, route, template, static_file, error
+import requests,datetime,os
+
 
 response = requests.get('http://apis.is/petrol')
 
+
 data = response.json()
+odyrtben= data["results"][0]
+for stod in data["results"]:
+    if stod["bensin95"] < odyrtben["bensin95"]:
+        odyrtben=stod
+
+odyrtdi= data["results"][0]
+for stod in data["results"]:
+    if stod["diesel"] < odyrtdi["diesel"]:
+        odyrtdi=stod
+t = data["timestampPriceCheck"]
+t = datetime.datetime.strptime(t,'%Y-%m-%dT%H:%M:%S.%f')
+t = t.strftime('%H:%M:%S %d/%m/%Y')
 
 @route('/')
 def index():
-    return template('index', data=data)
+    return template('smok',t=t,data=data,odyrtben=odyrtben,odyrtdi=odyrtdi)
 
-@route('/company/<c>')
-def company(c):
-    return template('company', c=c, data=data)
+@route('/company/<slod>')
+def company(slod):
+    til = False
+    for stod in data['results']:
+        if slod == stod['company']:
+            til = True
+            break
+    if til == True:
+        return template('fyrir', slod=slod , data=data)
+    else:
+        return '<h1>Síðan sem þú bast um er ekki til...'
 
-@route('/details/<key>')
-def details(key):
-    return template('details', key=key, data=data)
+
+@route('/info/<ok>')
+def info(ok):
+    return template('info',ok=ok, data=data)
+
+@error(404)
+@error(500)
+def error(error):
+    return '<h1>Síðan sem þú bast um er ekki til...'
 
 @route('/static/<filename>')
-def server_static(filename):
-    return static_file(filename, root='./')
+def static_server(filename):
+    return static_file(filename, root=('./static_files'))
 
-run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+run(host="0.0.0.0", port=int(os.environ.get("PORT")))
